e4841232d5f2599b0a1fafe1151970ebe2e168fd
orldloman <92477078+orldloman@users.noreply.github.com>
2025-04-16 20:55:06 -0600

    Add files via upload

---
+++ foto.js
@@ -0,0 +1,27 @@
+﻿const albumView = document.getElementById('album-view');
+const modalView = document.getElementById('modal-view');
+
+PHOTO_LIST.forEach(photoSrc => {
+  const img = document.createElement('img');
+  img.src = photoSrc;
+  img.addEventListener('click', () => showModal(photoSrc));
+  albumView.appendChild(img);
+});
+
+function showModal(src) {
+  const modalImg = document.createElement('img');
+  modalImg.src = src;
+  modalView.innerHTML = ''; // Limpiamos por si ya había una imagen
+  modalView.appendChild(modalImg);
+
+  modalView.style.display = 'flex'; // Aquí lo mostramos
+  modalView.style.top = window.pageYOffset + 'px';
+  document.body.classList.add('no-scroll');
+}
+
+// Cierra el modal al hacer clic en él
+modalView.addEventListener('click', () => {
+  modalView.style.display = 'none';
+  modalView.innerHTML = '';
+  document.body.classList.remove('no-scroll');
+});
